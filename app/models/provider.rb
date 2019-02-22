6db7fec6a7b06a20291e00cfe07356e0469fb693
Jose Antonio Parra <josparbar@gmail.com>
2019-02-22 11:20:24 +0100

    Relation of Provider with Tracking.

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -3,4 +3,9 @@ class Provider < ApplicationRecord
   # Validations
   # ==========================================================
   validates :name, presence: true
+
+  # ==========================================================
+  # Relations
+  # ==========================================================
+  has_many :trackings, dependent: :destroy
 end
