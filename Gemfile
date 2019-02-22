30781b4cf26670581b7584ed87548e281a7649c3
Jose Antonio Parra <josparbar@gmail.com>
2019-02-22 10:38:38 +0100

    Test User model.

--- Gemfile
+++ Gemfile
@@ -42,8 +42,8 @@ gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 gem 'devise', '~> 4.6', '>= 4.6.1'
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-rails'
+  gem 'faker'
 end
 
 group :development do
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid user' do
+    user = User.new(email: Faker::Internet.email, password: Faker::Internet.password(6))
+    assert user.valid?
+  end
+
+  test 'invalid without email' do
+    user = User.new(password: Faker::Internet.password(6))
+    refute user.valid?
+    assert_not_nil user.errors[:email]
+  end
+
+  test 'invalid without password' do
+    user = User.new(email: Faker::Internet.email)
+    refute user.valid?
+    assert_not_nil user.errors[:password]
+  end
+
+  test 'invalid with password length less than 6 characters' do
+    user = User.new(password: Faker::Internet.password(1, 5))
+    refute user.valid?
+    assert_not_nil user.errors[:password]
+  end
 end
