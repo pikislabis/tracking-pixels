70f39df1612a6e2bf1c9da728f63fb965821bab7
Jose Antonio Parra <josparbar@gmail.com>
2019-02-22 12:50:47 +0100

    Generate an API Token for the user when it's created.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  # ==========================================================
+  # Callbacks
+  # ==========================================================
+  after_initialize :assign_api_token
+
   def refresh_api_token
     self.update(api_token: SecureRandom.uuid)
+  end
+
+  private
+
+  def assign_api_token
+    self.api_token ||= SecureRandom.uuid
   end
 end
