28137dd6a008e0e16d26b7504c073ecd14c25d90
Jose Antonio Parra <josparbar@gmail.com>
2019-02-22 13:04:37 +0100

    Remove unused actions at trackings controller.

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -1,23 +1,9 @@
 class TrackingsController < ApplicationController
-  before_action :set_provider, only: %i(index new create destroy)
+  before_action :set_provider, only: %i(index destroy)
   before_action :set_tracking, only: :destroy
 
   def index
     @trackings = @provider.trackings
-  end
-
-  def new
-    @tracking = @provider.trackings.build
-  end
-
-  def create
-    @tracking = Tracking.new(tracking_params)
-
-    if @tracking.save
-      redirect_to provider_trackings_path(@provider)
-    else
-      render :new
-    end
   end
 
   def destroy
@@ -33,9 +19,5 @@ class TrackingsController < ApplicationController
 
   def set_tracking
     @tracking = Tracking.find(params[:id])
-  end
-
-  def tracking_params
-    params.require(:tracking).permit(:name)
   end
 end
