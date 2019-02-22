2686decf499735ca281a90485f0eb7f48170e251
Jose Antonio Parra <josparbar@gmail.com>
2019-02-22 12:36:33 +0100

    Method on user model to refresh its API Token

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  def refresh_api_token
+    self.update(api_token: SecureRandom.uuid)
+  end
 end
